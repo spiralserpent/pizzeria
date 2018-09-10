b7b59c6812e149f98d1ae1fed142a3a177b37f58
spiralserpent <33848092+spiralserpent@users.noreply.github.com>
2018-09-10 11:29:21 -0500

    pages about contact y colores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { AboutPage} from '../pages/about/about';
+import { MenuPage } from '../pages/menu/menu';
+import { ContactPage} from '../pages/contact/contact';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +25,10 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Menu', component: MenuPage },
+      { title: 'About', component: AboutPage },
+      { title: 'Contact', component: ContactPage }
     ];
 
   }
